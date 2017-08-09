f24449d0d54ffde6030a83208a5cdce8b9925db5
skamenialo <janowski.d@wp.pl>
2017-08-10 00:33:42 +0200

    Created new default MVC project

---
+++ ZPP_Project/ZPP_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZPP_Project.Startup))]
+namespace ZPP_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
