2ced8e62f32f0957a5561a6f5e0797134f063a95
xaoo99 <xaoo99@gmail.com>
2017-08-23 10:52:26 +0200

    Mad-quality logo included (somebody should work on that...). GlobalData class for static values created. Root layout altered.

---
+++ ZPP_Project/ZPP_Project/Models/GlobalData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZPP_Project.Models
+{
+    public static class GlobalData
+    {
+        public static string ApplicationName = "ZPP";
+    }
+}
