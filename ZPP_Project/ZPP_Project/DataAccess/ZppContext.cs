5038b1630f727845d47d4288a58d5bf5f7989935
xaoo99 <xaoo99@gmail.com>
2017-08-23 10:22:27 +0200

    EDM-related files moved to another directory. Local-database (file) connection string created. All views imported. ZppContext for data access created (not tested). Single model (Course) created.

---
+++ ZPP_Project/ZPP_Project/DataAccess/ZppContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ZPP_Project.Models;
+
+namespace ZPP_Project.DataAccess
+{
+    public class ZppContext : DbContext
+    {
+
+#region Properties
+
+        public DbSet<Course> Courses { get; set; }
+
+#endregion
+
+#region Constructor
+
+        public ZppContext()
+#if DATABASE_LOCAL
+            : base("ZPPEntities_NoDb")
+#elif DATABASE_REMOTE
+            : base("ZPPEntities")
+#endif
+        { }
+
+
+#endregion
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+        }
+    }
+
+}
---
+++ ZPP_Project/ZPP_Project/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZPP_Project.Models
+{
+    public class Course
+    {
+        public int IdCourse { get; set; }
+        public int IdTeacher { get; set; }
+        public int IdCompany { get; set; }
+        public string Name { get; set; }
+        public int Lectures { get; set; }
+        public int State { get; set; }
+        public string Description { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+    }
+}
