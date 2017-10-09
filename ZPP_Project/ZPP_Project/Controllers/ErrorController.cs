2269a0a9954f0e0a9f374f931c6d55d77661d60d
skamenialo <janowski.d@wp.pl>
2017-10-09 22:43:31 +0200

    Added custom unauthorized access

---
+++ ZPP_Project/ZPP_Project/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZPP_Project.DataAccess;
+using ZPP_Project.EntityDataModel;
+
+namespace ZPP_Project.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ZPP_Project/ZPP_Project/Helpers/ZPPAuthorizeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ZPP_Project.Helpers
+{
+    public class ZPPAuthorizeAttribute : AuthorizeAttribute
+    {
+        //
+        // Summary:
+        //     Processes HTTP requests that fail authorization.
+        //
+        // Parameters:
+        //   filterContext:
+        //     Encapsulates the information for using System.Web.Mvc.AuthorizeAttribute.
+        //     The filterContext object contains the controller, HTTP context, request context,
+        //     action result, and route data.
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Error");
+        }
+    }
+}
