18383138ea5a7a57423fa5f63a53075c2abace51
xaoo99 <xaoo99@gmail.com>
2017-10-21 19:06:25 +0200

    Minor code cleanup

--- ZPP_Project/ZPP_Project/Helpers/HtmlHelperZpp.cs
+++ ZPP_Project/ZPP_Project/Helpers/HtmlHelperZpp.cs
@@ -11,9 +11,6 @@ namespace ZPP_Project.Helpers
         public static MvcHtmlString RenderColapseIcon(string regionName)
         {
             MvcHtmlString result = new MvcHtmlString("<i class=\"fa fa-angle-right\" aria-hidden=\"true\" id=\"" + regionName + "_toggle\" onclick=\"ToggleVisibilityState('" + regionName + "')\"></i>");
-            //onclick="ToggleVisibilityState(regionName)"function() {$( "regionName" ).slideToggle( "slow"} )
-            //<i class="fa fa-angle-down" aria-hidden="true"></i>ToggleVisibilityState(regionName)
-
             return result;
         }
     }
