cced13e98609d25ae6ccad56751c74f72aacafcd
xaoo99 <xaoo99@gmail.com>
2018-01-24 18:44:54 +0100

    Collapse-expand widened. VERY basic css applied.

--- ZPP_Project/ZPP_Project/Helpers/ZPPHtmlHelper.cs
+++ ZPP_Project/ZPP_Project/Helpers/ZPPHtmlHelper.cs
@@ -10,7 +10,7 @@ namespace ZPP_Project.Helpers
     {
         public static MvcHtmlString RenderColapseIcon(string regionName)
         {
-            MvcHtmlString result = new MvcHtmlString("<span class=\"fa fa-angle-right\" data-toggle=\"collapse\" id=\"" + regionName + "_toggle\" data-target=\"#" + regionName + "\"></span>");
+            MvcHtmlString result = new MvcHtmlString("<span class=\"fa fa-angle-right visibility-toggle-button\" data-toggle=\"collapse\" id=\"" + regionName + "_toggle\" data-target=\"#" + regionName + "\"></span>");
             return result;
         }
     }
